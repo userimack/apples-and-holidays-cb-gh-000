b504fdd4753e7d10be60a43a2b8c4c980b8a8c68
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-01 17:22:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, events|
     puts "#{season}:"
     events.each do |event, supply|
-      puts "#{event}: #{supply.join(", ")}"
+      puts "#{event[0].upcase + event.slice(1, event.length)}: #{supply.join(", ")}"
     end 
   end 
 end
