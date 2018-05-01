146dba6974db60867cd54cfdb5ed9efefea18f5f
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-01 17:19:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, events|
-    puts "#{season}"
+    puts "#{season}:"
     events.each do |event, supply|
+      puts "#{event}: #{supply.join(", ")}"
       
 
 end
